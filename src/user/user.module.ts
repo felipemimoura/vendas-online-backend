4adf18cc19dedc9fe23fe948af8a26cf7708abe6
Felipe Moura <felipemimoura@gmail.com>
2023-11-09 20:13:35 -0300

    feat: create a service to save data

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 // Create a new Module to project
 // command: nest g module <<moduleName>>
 @Module({
   controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
