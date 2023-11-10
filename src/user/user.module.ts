663daf6618762ac1969bb54e1c95262a96b200e7
Felipe Moura <felipemimoura@gmail.com>
2023-11-09 21:41:34 -0300

    feat: configure typeorm module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './interfaces/user.entity';
 
 // Create a new Module to project
 // command: nest g module <<moduleName>>
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
