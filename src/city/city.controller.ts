1db16adcc0a108d9f9ce8668f2e746589855d9b5
Felipe moura <felipe.moura@pactoenergia.com.br>
2023-11-14 18:46:46 -0300

    feat: implements cache strategy to get data

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,14 +1,35 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { CityService } from './city.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Controller('city')
 export class CityController {
-  constructor(private readonly cityService: CityService) {}
+  constructor(
+    private readonly cityService: CityService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
   @Get('/:stateId')
   async getAllCitiesByStateId(
     @Param('stateId') stateId: number,
   ): Promise<CityEntity[]> {
-    return this.cityService.getAllCitiesByStateId(stateId);
+    const cachedCities: CityEntity[] = await this.cacheManager.get(
+      `state_${stateId}`,
+    );
+
+
+    // Check if has cities on cache
+    if (cachedCities) {
+      return cachedCities;
+    }
+
+    // Get all cities
+    const cities = await this.cityService.getAllCitiesByStateId(stateId);
+
+    //Save data on Cache
+    await this.cacheManager.set(`state_${stateId}`, cities);
+
+    return cities;
   }
 }
