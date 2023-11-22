3435c6de8215c17c13edf5351d054a8ca7a0d71b
Felipe moura <felipe.moura@pactoenergia.com.br>
2023-11-22 08:17:18 -0300

    feat: add validation to DTO on create user

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,7 +1,14 @@
-export interface CreateUserDTO {
+import { IsString } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
   name: string;
+  @IsString()
   email: string;
+  @IsString()
   password: string;
+  @IsString()
   phone: string;
+  @IsString()
   cpf: string;
 }
